66c914a482ca2b662fa191ff0fe511420bbe49ff
”tangshunyao” <211449757@qq.com>
2025-02-25 15:14:06 +0800

    change pizza

--- src/App.java
+++ src/App.java
@@ -1,10 +1,7 @@
 public class App {
-    public static pizza takeOrder(String orderId, String ingredients, String sides, String drink, String prices, String total) {
-        pizza pizza = new pizza(); 
-        return pizza;
+    public static void main(String[] args){
+          sliceoheavenpizza pizza1 = new sliceoheavenpizza();
+          pizza1.takeOrder("AX0187", "cheese", "16","milk",25,25.00);
+        }
     }
-    public static void main(String[] args) throws Exception {
-        pizza pizza1 = new pizza();
-        pizza1 = takeOrder("1234", "cheese", "12", "milk", "20", "20");
-    }
-}
+      
---
+++ src/sliceoheavenpizza.java
@@ -0,0 +1,66 @@
+public class sliceoheavenpizza {
+        public String storeAddress;
+        public String storeName;
+    
+        public String Menu;
+    
+        public long storePhone;
+        public String storEemali;
+        public String pizzaIngredients;
+        public double pizzaPrice;
+        public String pizzaSide;
+        public String drinks;
+    
+        private String orderID;
+        private double orderTotal;
+    
+        public final static String DEF_ORDER_ID = "DEF_SOH_099";
+        public final static String DEF_PIZZA_INGREDIENTS = "Mozzarella pizza";
+        public final static double DEF_ORDER_TOTAL = 15.00;
+
+        public sliceoheavenpizza(){
+            this.orderID = DEF_ORDER_ID;
+            this.pizzaIngredients = DEF_PIZZA_INGREDIENTS;
+            this.orderTotal = DEF_ORDER_TOTAL;
+
+        }
+        public void takeOrder(String id, String ingredients, String sides, String drink, double price, double total){
+            this.orderID = id;
+            this.pizzaIngredients = ingredients;
+            this.pizzaSide = sides;
+            this.drinks = drink;
+            this.pizzaPrice = price;
+            this.orderTotal = total;
+            
+            System.out.println("order accepted!");
+    
+            System.out.println("order is being prepared");
+    
+            try{
+                Thread.sleep(5000);
+            } catch(InterruptedException e){
+                System.out.println("order is ready for pickup!");
+            }
+    
+            System.out.println("your order is ready!");
+             
+            printreceipt();
+        
+        }
+    
+        private void printreceipt(){
+            System.out.println("********RECEIPT********");
+    
+            System.out.println("order ID:" + orderID);
+            System.out.println("order total:" + orderTotal);
+        
+        }
+    
+        public void makepizza(){
+            System.out.println("pizza ingredients:"  + pizzaIngredients);
+            System.out.println("pizza sides:"  + pizzaSide);
+            System.out.println("drinks:"  + drinks);
+        } 
+    
+    }
+    
